cbabb3b8d58ed47f506b2d089718744dcfc510aa
Thiago_Ianzer <60183103+ustop0@users.noreply.github.com>
2022-12-14 18:22:13 -0300

    Atualizando função de envio de e-mails

--- VB6/ftaEnviarEmailCDO.vb
+++ VB6/ftaEnviarEmailCDO.vb
@@ -1,9 +1,9 @@
 '---------------------------------------------------------------------------------------'
-'Procedure: ftaEnviarEmailCDO                                                           '
-'Autor: Thiago                                                                          '
+'Função: ftaEnviarEmailCDO                                                              '
+'Autor: Thiago Ianzer                                                                   '
 'Data: 04/07/2022                                                                       '
 'Propósito: Enviar com ou sem anexo                                                     '
-'Observação: É necessário o componente Microsoft CDO to Windows 2000                    '
+'Observação: É necessário o componente Microsoft CDO to Windows 2000 Library            '
 '---------------------------------------------------------------------------------------'
 Public Function ftaEnviarEmailCDO(strClienteSMTP As String _
                                  , strPortaSMTP As Integer _
@@ -20,6 +20,17 @@ Public Function ftaEnviarEmailCDO(strClienteSMTP As String _
     
 If Not blnDebug = True Then On Error GoTo Erro
 
+'Removendo caracteres especiais do email e espaços
+    strUsuario_email = Trim(ftaRegexEmail(strUsuario_email))
+    strUsuario_email = Replace(strUsuario_email, ";", ",")
+    
+    strRemetente_email = Trim(ftaRegexEmail(strRemetente_email))
+    strRemetente_email = Replace(strRemetente_email, ";", ",")
+
+    strNomeRemetente_email = Trim(ftaRegexEmail(strNomeRemetente_email))
+    strNomeRemetente_email = Replace(strNomeRemetente_email, ";", ",")
+    
+    
     On Error Resume Next 'Set up error checking
     Set cdoMsg = CreateObject("CDO.Message")
     Set cdoConf = CreateObject("CDO.Configuration")
@@ -46,8 +57,8 @@ If Not blnDebug = True Then On Error GoTo Erro
          .HTMLBody = strMenssagem_email ' Body of message can be any HTML code
             
     'Verificando copia oculta e anexo
-         If strCC_email <> "" Then .CC = strCC
-         If strCCO_email <> "" Then .BCC = strBcc
+         If strCC_email <> "" Then .CC = strCC_email
+         If strCCO_email <> "" Then .BCC = strCCO_email
          If strAnexo_email <> "" Then .AddAttachment strAnexo_email
             
          Set .Configuration = cdoConf
@@ -70,3 +81,4 @@ If Not blnDebug = True Then On Error GoTo Erro
 Erro:
     Call ftaTrataErro
 End Function
+                      
